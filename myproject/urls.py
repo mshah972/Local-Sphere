5e37fa7c8c2b49318f083042b1e3100fad9b079b
Wina-ui <dhwangdepaul@gmail.com>
2025-02-21 22:08:54 -0600

    Added a sphereai page with input options as well as a button to submit

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,6 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('SphereAi', views.SphereAi, name='SphereAi')
 ]
 
 
--- myproject/views.py
+++ myproject/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def SphereAi(request):
+    return render(request, 'SphereAi.html')
